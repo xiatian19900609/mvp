714b9536c16a61bb0139dd8a57cb76722b13e92a
xiatian <tian.xia@8win.com>
2017-03-20 17:29:31 +0800

    11

--- MVP/app/src/main/java/summer/zy/com/mvp/injector/components/HomeComponent.java
+++ MVP/app/src/main/java/summer/zy/com/mvp/injector/components/HomeComponent.java
@@ -1,7 +1,6 @@
 package summer.zy.com.mvp.injector.components;
 
 import dagger.Component;
-import summer.zy.com.mvp.injector.PerActivity;
 import summer.zy.com.mvp.injector.modules.HomeModule;
 import summer.zy.com.mvp.module.home.HomeActivity;
 
@@ -10,7 +9,6 @@ import summer.zy.com.mvp.module.home.HomeActivity;
  *
  * @author summer
  */
-@PerActivity
 @Component(modules = HomeModule.class)
 public interface HomeComponent {
     void inject(HomeActivity activity);
--- MVP/app/src/main/java/summer/zy/com/mvp/injector/modules/HomeModule.java
+++ MVP/app/src/main/java/summer/zy/com/mvp/injector/modules/HomeModule.java
@@ -2,7 +2,6 @@ package summer.zy.com.mvp.injector.modules;
 
 import dagger.Module;
 import dagger.Provides;
-import summer.zy.com.mvp.injector.PerActivity;
 import summer.zy.com.mvp.module.base.IBasePresenter;
 import summer.zy.com.mvp.module.home.HomeActivity;
 import summer.zy.com.mvp.module.home.HomePresenter;
@@ -20,7 +19,6 @@ public class HomeModule {
         this.mView = mView;
     }
 
-    @PerActivity
     @Provides
     public IBasePresenter provideHomePresenter() {
         return new HomePresenter(mView, mView);
